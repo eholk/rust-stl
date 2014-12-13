36767c6c192bbb916bb37b93a74b58da3eadc0ee
Eric Holk <eric.holk@gmail.com>
2014-12-13 01:55:27 -0500

    Defining some structs for reading in files.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,21 @@
+struct Triangle {
+    normal: [f32, ..3],
+    v1: [f32, ..3],
+    v2: [f32, ..3],
+    v3: [f32, ..3],
+    attr_byte_count: u16
+}
+
+struct BinaryStlHeader {
+    header: [u8, ..80],
+    num_triangles: u32
+}
+
+struct BinaryStlFile {
+    header: BinaryStlHeader,
+    triangles: [Triangle]
+}
+
 #[test]
 fn it_works() {
 }
